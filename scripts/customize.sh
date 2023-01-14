4a93d4667eaee7136de8c5dbf02f16c07b0273d5
thrwKappu <31341536+thrwKappu@users.noreply.github.com>
2023-01-14 10:32:48 +0700

    upstream sync

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -2,7 +2,9 @@
 
 if [ $ARCH = "arm" ]; then
 	alias cmpr='$MODPATH/bin/arm/cmpr'
+	ARCH_LIB=armeabi-v7a
 elif [ $ARCH = "arm64" ]; then
+	ARCH_LIB=arm64-v8a
 	alias cmpr='$MODPATH/bin/arm64/cmpr'
 else
 	abort "ERROR: unsupported arch: ${ARCH}"
@@ -28,14 +30,23 @@ if [ -n "$BASEPATH" ] && cmpr $BASEPATH $MODPATH/__PKGNAME.apk; then
 else
 	ui_print "  * Updating __PKGNAME to __PKGVER"
 	set_perm $MODPATH/__PKGNAME.apk 1000 1000 644 u:object_r:apk_data_file:s0
-	if ! op=$(pm install --user 0 -i com.android.vending -r -d $MODPATH/__PKGNAME.apk 2>&1); then
+	if ! op=$(cmd package install --user 0 -i com.android.vending -r -d $MODPATH/__PKGNAME.apk 2>&1); then
 		ui_print "ERROR: APK installation failed!"
-		abort "${op}"
+		abort "$op"
 	fi
 	BASEPATH=$(basepath)
 	if [ -z "$BASEPATH" ]; then
 		abort "ERROR: install __PKGNAME manually and reflash the module"
 	fi
+fi
+BASEPATHLIB=${BASEPATH%base.apk}lib/${ARCH}
+if ! ls ${BASEPATHLIB}/*.so; then
+	ui_print "  * Extracting native libs"
+	if ! op=$(unzip -j $MODPATH/__PKGNAME.apk lib/${ARCH_LIB}/* -d ${BASEPATHLIB} 2>&1); then
+		ui_print "ERROR: extracting native libs failed"
+		abort "$op"
+	fi
+	set_perm_recursive ${BASEPATHLIB} 1000 1000 755 755 u:object_r:apk_data_file:s0
 fi
 ui_print "  * Setting Permissions"
 set_perm $MODPATH/base.apk 1000 1000 644 u:object_r:apk_data_file:s0
@@ -48,10 +59,10 @@ if ! op=$(mount -o bind $RVPATH $BASEPATH 2>&1); then
 	ui_print "ERROR: Mount failed!"
 	abort "$op"
 fi
-rm -r $MODPATH/bin $MODPATH/__PKGNAME.apk
 am force-stop __PKGNAME
-
 ui_print "  * Optimizing __PKGNAME"
 cmd package compile --reset __PKGNAME &
+
+rm -r $MODPATH/bin $MODPATH/__PKGNAME.apk
 
 ui_print "  * Finished"
