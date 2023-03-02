6e63c01e3e24d7cdaf72a25a6906b9f02e048e43
thrwKappu <31341536+thrwKappu@users.noreply.github.com>
2023-03-03 01:42:38 +0700

    upstream sync

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -5,4 +5,9 @@
 	NVBASE=${MODULES%/*}
 	rm $NVBASE/rvcmm/__PKGNAME_rv.apk
 	rmdir $NVBASE/rvcmm
+	if __ISBNDL :; then
+		until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
+		sleep 15
+		pm uninstall __PKGNAME
+	fi
 } &
