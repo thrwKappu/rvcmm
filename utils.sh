2bbcf9324e2ec3c36a61cb6c5acc7deb93549e9e
thrwKappu <31341536+thrwKappu@users.noreply.github.com>
2023-08-30 20:53:20 +0700

    oops

--- utils.sh
+++ utils.sh
@@ -454,8 +454,8 @@ build_rv() {
 		service_sh "$pkg_name" "$version" "$base_template"
 		customize_sh "$pkg_name" "$version" "$arch" "$extrct" "$base_template"
 
-		local patches_string = "(Everything)"
-		if [ -n "${args[included_patches]}" ]; then "${args[included_patches]}" fi
+		local patches_string="(All)"
+		if [ -n "${args[included_patches]}" ]; then patches_string="${args[included_patches]}" fi
 		module_prop \
 			"${args[module_prop_name]}" \
 			"RVCMM: ${app_name} - ${arch}" \
