f0f4f2b94419b205b09791795a2de6997fc25ea1
thrwKappu <31341536+thrwKappu@users.noreply.github.com>
2026-06-14 09:36:17 +0700

    108 test

--- utils.sh
+++ utils.sh
@@ -242,7 +242,7 @@ _req() {
 		mv -f "$dlp" "$op"
 	fi
 }
-req() { _req "$1" "$2" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:147.0) Gecko/20100101 Firefox/147.0"; }
+req() { _req "$1" "$2" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0"; }
 gh_req() { _req "$1" "$2" -H "$GH_HEADER"; }
 gh_dl() {
 	if [ ! -f "$1" ]; then
